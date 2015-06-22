1aa4269409c0ac4f32e6b4c0a0b881b1394f9506
Rex Su <rex.dongyu.su@gmail.com>
2015-06-22 18:24:38 -0400

    Added slider animation.

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,39 @@
 (function () {
     'use strict';
 
+    /*
+        Quote slider
+    */
+    function resetQuoteSlider() {
+        $('.testimonials ._quotes ._quote')
+            .removeClass('-prev')
+            .removeClass('-current')
+            .addClass('-next');
+
+        $('.testimonials ._quotes ._quote:first-child')
+            .removeClass('-next')
+            .addClass('-current');
+    }
+
+    function playQuoteSlider() {
+        var currentQuote = $('.testimonials ._quotes ._quote.-current');
+        var nextQuote = currentQuote.next();
+
+        currentQuote
+            .removeClass('-current')
+            .addClass('-prev');
+        nextQuote
+            .removeClass('-next')
+            .addClass('-current');
+    }
+
+    setInterval(function() {
+        var nextQuote = $('._quote.-current').next();
+
+        if (nextQuote.size()) {
+            playQuoteSlider();
+        } else {
+            resetQuoteSlider();
+        }
+    }, 3000);
 })();
